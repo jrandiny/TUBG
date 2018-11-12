55e63f068c5c1cca56cbfdfe8f3b2aebc31de01f
jrandiny <jrandiny2@gmail.com>
2018-11-12 20:43:40 +0700

    Fix printMap
    Co-authored-by: a-johanes <Antoniusjohanes99@gmail.com>
    Co-authored-by: willysantoso05 <willysantoso05@gmail.com>
    Co-authored-by: SteveImmanuel <steveandreasimanuel@gmail.com>

--- print.pl
+++ print.pl
@@ -2,15 +2,13 @@
 printXHoriz(0):-nl.
 printXHoriz(Count):-write('X'),NextCount is Count -1, printXHoriz(NextCount).
 
-printMap(X,Y):- Y>12,!,fail.
-printMap(X,Y):- getTopLeft(_,YPojok),Y<YPojok,printXHoriz(12),!,printMap(1, Y+1).
-printMap(X,Y):- getBottomRight(_,YBawah), Y>YBawah,printXHoriz(12),!,printMap(1,Y+1).
+printMap(X,Y):- Y>12,!.
+printMap(X,Y):- getTopLeft(_,YPojok),Y<YPojok,printXHoriz(33),!,printMap(1, Y+1).
+printMap(X,Y):- getBottomRight(_,YBawah), Y>YBawah,printXHoriz(33),!,printMap(1,Y+1).
 printMap(X,Y):- X>12,nl,!,printMap(1,Y+1).
-printMap(X,Y):- isInside(X,Y),write('O'),!,printMap(X+1,Y).
-printMap(X,Y):- write('X'),!,printMap(X+1,Y).
-printMap(X,Y):- X>12,!,fail.
-
-
+printMap(X,Y):- isInside(X,Y),write(' - '),!,printMap(X+1,Y).
+printMap(X,Y):- X=1,write('X'),!,printMap(X+1,Y).
+printMap(X,Y):- write(' X '),!,printMap(X+1,Y).
 
 status:- write('Health: '),
         pHealth(Health),
